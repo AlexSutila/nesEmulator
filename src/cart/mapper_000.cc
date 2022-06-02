5bb70acb8979924331e9ab6055224467aea82fc1
AlexSutila <96510931+AlexSutila@users.noreply.github.com>
2022-06-01 19:29:26 -0700

    Remove PRGRAM from mapper 000
    I guess mapper 000 just doesn't have it ???

--- src/cart/mapper_000.cc
+++ src/cart/mapper_000.cc
@@ -1,31 +1,9 @@
 #include <assert.h>
 #include "cart/mapper.hh"
 
+#include <iostream>
+    
 void Mapper_000::cpu_WB(uint16_t addr, uint8_t value) {
-
-    // PRG RAM
-    if (addr >= 0x6000 && addr <= 0x7FFF) {
-
-        // One bank - mirrored to fill the 8 KiB range accordingly
-        if (m_size_prg_ram == 0x1000) {
-
-            int index = (addr - 0x6000) & 0x0FFF;
-            assert(index >= 0x0000 && index <= 0x0FFF);
-            m_cart->get_PRG_RAM()[index] = value;
-
-        }
-        else if (m_size_prg_ram == 0x0800) {
-            
-            int index = (addr - 0x6000) & 0x07FF;
-            assert(index >= 0x0000 && index <= 0x07FF);
-            m_cart->get_PRG_RAM()[index] = value;
-
-        }
-
-        // This is either rare or does not exist
-        else assert(false);
-
-    }
 
 }
 
@@ -49,30 +27,6 @@ uint8_t Mapper_000::cpu_RB(uint16_t addr) {
             int index = (addr - 0x8000) & 0x3FFF;
             assert(index >= 0x0000 && index <= 0x3FFF);
             return m_cart->get_PRG_ROM()[index];
-
-        }
-
-        // This is either rare or does not exist
-        else assert(false);
-
-    }
-
-    // PRG RAM
-    else if (addr >= 0x6000 && addr <= 0x7FFF) {
-
-        // One bank - mirrored to fill the 8 KiB range accordingly
-        if (m_size_prg_ram == 0x1000) {
-
-            int index = (addr - 0x6000) & 0x0FFF;
-            assert(index >= 0x0000 && index <= 0x0FFF);
-            return m_cart->get_PRG_RAM()[index];
-
-        }
-        else if (m_size_prg_ram == 0x0800) {
-            
-            int index = (addr - 0x6000) & 0x07FF;
-            assert(index >= 0x0000 && index <= 0x07FF);
-            return m_cart->get_PRG_RAM()[index];
 
         }
 
